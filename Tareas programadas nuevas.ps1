e21e8c3740f24c7a78e5dabe0211637ba8fc1556
Staga <54549529+Staga1996@users.noreply.github.com>
2023-03-21 13:41:07 +0100

    [POWERSHELL] CREACIÓN TAREAS PROGRAMADAS

---
+++ Tareas programadas nuevas.ps1
@@ -0,0 +1,6 @@
+﻿#Crear tareas programadas en windows
+
+$trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Sunday -At 12pm
+$action = New-ScheduledTaskAction -Execute 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe' -Argument [añade la ruta del archivo el script que deseas que se ejecute como tarea programada]
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Limpieza del Cache" -Description "Script de limpieza de archivos temporales del equipo"
